a9b07278395b4ed3e518b9d18099d302c9002e02
nanjizal <nanjizal@users.noreply.github.com>
2018-11-19 06:15:58 +0000

    added test of reconstructing TP.

--- src/Main.hx
+++ src/Main.hx
@@ -6,19 +6,27 @@ import hxRectPack2D.pixels.Helper;
 import hxRectPack2D.atlas.AtlasBuilder;
 import folder.Folder;
 
+import hxRectPack2D.output.TP;
+//import haxe.Json;
+
 #if neko
 import neko.vm.Module;
 #end
 
 class Main {
-    var assets = './assets/';
-    var outputFolder = 'rectPack';
+    var assets           = './assets/';
+    var outputFolder     = 'rectPack';
+    var outputImageName  = 'zebraAtlas.png';
+    var outputJSONname   = 'zebra_Atlas.json';
+    var newFolder: String;
     var folder = new Folder();
     public static 
     function main() { new Main(); }
     public
     function new(){
+        newFolder = assets + outputFolder;
         loadAndPrepareImages();
+        //reconstruct();
     }
     function loadAndPrepareImages(){
         var arrImgSpec  = folder.getImages( assets );
@@ -38,11 +46,16 @@ class Main {
         var width = atlasBuilder.generatePackingData( cast arrImages, names, height );
         Sys.println( 'width ' + width );
         var outputPng = new Pixels( width, height, true );
-        var newFolder = assets + outputFolder;
         folder.deleteDirectory( newFolder );
         folder.createDirectory( newFolder );
-        var atlas: Atlas = atlasBuilder.getAtlas( newFolder +'/zebraAtlas.png', cast new Wrapper( outputPng ) );
-        folder.savePNG( outputPng, newFolder + '/zebraAtlas.png' );
-        folder.saveText( newFolder +'/zebra_Atlas.json', atlas.jsonString );
+        var atlas: Atlas = atlasBuilder.getAtlas( newFolder + '/' + outputImageName, cast new Wrapper( outputPng ) );
+        folder.savePNG( outputPng, newFolder + '/' + outputImageName );
+        folder.saveText( newFolder + '/' + outputJSONname, atlas.jsonString );
+        
+        var s = folder.loadText( newFolder + '/' + outputJSONname );
+        var framesHolder = TP.reconstruct( s );
+        var tp = TP.frameHolderToTP( framesHolder );
+        Sys.println( tp.write() );
+        TP.frameHolderTraceImages( framesHolder );
     }
 }
